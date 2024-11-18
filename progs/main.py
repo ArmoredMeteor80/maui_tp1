7c2404ec91beff13aff7eb60ddbdcdc8bdb4c450
Mathieu TULOUP <mathieu.tuloup@gmail.com>
2024-11-18 09:09:11 +0100

    cesar_simple added

--- progs/main.py
+++ progs/main.py
@@ -1,4 +1,5 @@
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+LEN_ALPHA = len(ALPHABET)
 
 
 def find(char: str) -> int:
@@ -11,7 +12,18 @@ def reverse(msg: str) -> str:
     return msg[::-1]
 
 
+def cesar_simple() -> str:
+    """Renvoie le message chiffré selon la méthode de César simple"""
+    # Chiffrement ou déchiffrement ?
+    mode = int(input("Quel mode, chiffrement (1) ou déchiffrement (2) : "))
+    b_key = int(input(f"Clé de {"chiffrement" if mode == 1 else "déchiffrement"} : "))
+    msg = input(f"Message à {"chiffrer" if mode == 1 else "déchiffrer"} : ")
+    # Chiffrement
+    msg_y = ""
+    for char in msg:
+        msg_y += ALPHABET[(find(char) + b_key) % LEN_ALPHA] if mode == 1 else ALPHABET[(find(char) - b_key) % LEN_ALPHA]
+    return msg_y
+
+
 if __name__ == '__main__':
-    print(find("A"))
-    msg = input("Saisir message : ")
-    print(reverse(msg))
+    print(cesar_simple())
