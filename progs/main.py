188b410b1221a4633df3601f43964fa57b8aec6d
Mathieu TULOUP <mathieu.tuloup@gmail.com>
2024-11-18 08:42:26 +0100

    Added reverse function

--- progs/main.py
+++ progs/main.py
@@ -2,8 +2,16 @@ ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
 
 def find(char: str) -> int:
+    """Renvoie le code entier d'un caractère dans l'alphabet"""
     return ALPHABET.index(char)
+
+
+def reverse(msg: str) -> str:
+    """Chiffre un message par son écriture à l'envers"""
+    return msg[::-1]
 
 
 if __name__ == '__main__':
     print(find("A"))
+    msg = input("Saisir message : ")
+    print(reverse(msg))
