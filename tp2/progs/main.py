9eba0dc17b235cb27a026868888e8dfca81991ff
Mathieu TULOUP <mathieu.tuloup@gmail.com>
2024-12-17 14:23:01 +0100

    deleted useless comments and lines

--- tp2/progs/main.py
+++ tp2/progs/main.py
@@ -41,9 +41,9 @@ def dechiffrer_sad(msg: int, a: list, d: int, n: int) -> str:
     if est_supercroissante(a):
         msg_dechiffre = ""
         # On détermine C la capacité étant le message chiffré * le faciliteur
-        c = (d*msg)%n
+        c = (d * msg) % n
         n = len(a)
-        for i in range(n-1, -1, -1):
+        for i in range(n - 1, -1, -1):
             if c >= a[i]:
                 c -= a[i]
                 msg_dechiffre = "1" + msg_dechiffre
@@ -51,7 +51,6 @@ def dechiffrer_sad(msg: int, a: list, d: int, n: int) -> str:
                 msg_dechiffre = "0" + msg_dechiffre
         if c == 0:
             return msg_dechiffre
-    # Sinon faire chiffre de Merkle
     return "Il n'y a pas de solution avec algo glouton"
 
 
@@ -59,11 +58,10 @@ def chiffrer_sad(msg: str, b: list, n: int) -> int:
     """Renvoie le message chiffré d'après la clé publique : SAD difficile b et le module n"""
     msg_chiffre = ""
     for i in range(0, len(msg), len(b)):
-        bloc = msg[i:i+len(b)]
+        bloc = msg[i:i + len(b)]
         bloc_chiffre = 0
         for k in range(len(bloc)):
             bloc_chiffre += int(bloc[k]) * b[k]
-        print(bloc_chiffre)
         msg_chiffre += str(bloc_chiffre)
     return int(msg_chiffre)
 
@@ -105,43 +103,8 @@ if __name__ == '__main__':
     msg_chiffre = chiffrer_sad(msg_a_chiffrer, B, N)
     msg_dechiffre = dechiffrer_sad(msg_chiffre, A, D, N)
     print(f"le msg_chiffre est {msg_chiffre} et le msg_dechiffre est {msg_dechiffre}")
+    print(f"Pour le message chiffré 262257139 : {dechiffrer_sad(262257139, A, D, N)}")
+    print(f"Pour le message chiffré 232680541 : {dechiffrer_sad(232680541, A, D, N)}")
 
     print()
-
-    # ex 6 td6
-    A3 = [2, 3, 17, 25, 91]
-    print(f"est un SAD facile : {est_supercroissante(A3)}")
-    N3 = 139
-    print(f"N = {N3} est il acceptable : {est_mod_acceptable(A3, N3)}")
-    E3 = 17
-    print(f"E = {E3} est acceptable : {est_compliqueur_acceptable(N3, E3)}")
-    B3 = SAD_difficile(A3, N3, E3)
-    print(f"SAD difficile est : {B3}")
-    D3 = determine_faciliteur(E3, N3)
-    print(f"Faciliteur D vaut : {D3}")
-    print(f"partie publique, N : {N3}, B :{B3}")
-    print(f"partie privée, A : {A3}, D :{D3}")
-    msg_chiffre3 = 93
-    print(dechiffrer_sad(msg_chiffre3, A3, D3, N3))
-
-    # ex de con
-    A4 = [1, 3, 6]
-    E4 = 7
-    N4 = 12
-    print(chiffrer_sad("101", SAD_difficile(A4, N4, E4), N4))
-    print(dechiffrer_sad(13, A4, determine_faciliteur(E4, N4), N4))
-
-    print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAA!!!!')
-    # ex de con 2
-    A5 = [3, 4, 9, 19, 38, 77]
-    E5 = 27
-    N5 = 155
-    print(chiffrer_sad("001110", SAD_difficile(A5, N5, E5), N5))
-    print(dechiffrer_sad(232, A5, determine_faciliteur(E5, N5), N5))
-    print()
-    print(dechiffrer_sad(chiffrer_sad("001110", SAD_difficile(A5, N5, E5), N5), A5, determine_faciliteur(E5, N5), N5))
-
-
-
-
 
